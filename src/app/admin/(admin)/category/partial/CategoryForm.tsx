e837ae7972a0e91aa823d5652eaf59d942a2202b
Pratik <pratiikpatel.001@gmail.com>
2025-10-13 13:08:18 +0530

    Update Category Modal

--- src/app/admin/(admin)/category/partial/CategoryForm.tsx
+++ src/app/admin/(admin)/category/partial/CategoryForm.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { useCreateCategory } from '../hooks/useCreateCategory';
 import InputText from '@/components/forms/InputText';
 import { Button } from '@/components/ui/button';
+import InputTextArea from '@/components/forms/InputTextArea';
 
 const CategoryForm = () => {
   const formik = useCreateCategory()
@@ -11,7 +12,7 @@ const CategoryForm = () => {
       <form>
         <div>
           <InputText label='Category Name' name='category' />
-          <input type="text" placeholder='Description' />
+          <InputTextArea label='Description' name='category' />
           <Button className='mt-10' variant={'default'} onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
             e.preventDefault()
             formik.createCategoryFormik.handleSubmit()
